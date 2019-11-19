6cb16ba4f76e0dbb9f0b088126dc1838f49c6b95
ColtDahl <dahlcolt@gmail.com>
2019-11-18 19:46:43 -0600

    Replace threadedminimumpairwisedistance with serialminimumpairwisedistance, for now---tests pass.

--- src/mpd/ThreadedMinimumPairwiseDistance.java
+++ src/mpd/ThreadedMinimumPairwiseDistance.java
@@ -4,7 +4,16 @@ public class ThreadedMinimumPairwiseDistance implements MinimumPairwiseDistance 
 
     @Override
     public long minimumPairwiseDistance(int[] values) {
-        throw new UnsupportedOperationException();
-    }
-
+            long result = Integer.MAX_VALUE;
+            for (int i = 0; i < values.length; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    // Gives us all the pairs (i, j) where 0 <= j < i < values.length
+                    long diff = Math.abs(values[i] - values[j]);
+                    if (diff < result) {
+                        result = diff;
+                    }
+                }
+            }
+            return result;
+        }
 }
